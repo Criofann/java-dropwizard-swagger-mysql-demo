bd6ad886da5a5dd0cd4d42ab39f0687145a43508
Criofan McCarney-Savage <crio@c02fl0glml85.client.kainos.com>
2023-09-19 11:48:26 +0100

    US001 - Create a view products

---
+++ src/main/java/org/kainos/ea/resources/OrderController.java
@@ -0,0 +1,13 @@
+package org.kainos.ea.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api")
+public class OrderController {
+    @GET
+    @Path("/orders")
+    @Produces
+}
